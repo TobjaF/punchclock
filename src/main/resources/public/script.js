8a2b3139d1e8228a7eddee5601e27c33fe91b5e7
TobjaF <74833361+TobjaF@users.noreply.github.com>
2021-09-06 16:12:02 +0200

    Client-Logik zu CRUD von Entitäten "Rapport" und "Project" hinzugefügt
    - Diverse neue Funktionen in "script.js"
    -  "login.js" cleanup

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,8 +1,17 @@
 const URL = 'http://localhost:8081';
-let entries = [];
 let users = [];
-let mode = 'create';
+
+let entries = [];
+let projects = [];
+let rapports = [];
+
+let entryMode = 'create';
+let projectMode = 'create';
+let rapportMode = 'create';
+
 let currentEntry;
+let currentProject;
+let currentRapport;
 
 const dateAndTimeToDate = (dateString, timeString) => {
     return new Date(`${dateString}T${timeString}`).toISOString();
@@ -18,9 +27,6 @@ const getHeaders = () => {
 
 // API Requests
 const createEntry = (entry) => {
-    console.log('entry');
-    console.log(entry);
-    console.log(JSON.stringify(entry));
     let headers = getHeaders();
     headers['Content-Type'] =  'application/json';
     fetch(`${URL}/entries`, {
@@ -31,6 +37,36 @@ const createEntry = (entry) => {
         result.json().then((entry) => {
             entries.push(entry);
             renderEntries();
+        });
+    });
+};
+
+const createProject = (project) => {
+    let headers = getHeaders();
+    headers['Content-Type'] =  'application/json';
+    fetch(`${URL}/projects`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(project)
+    }).then((result) => {
+        result.json().then((project) => {
+            projects.push(project);
+            renderProjects();
+        });
+    });
+};
+
+const createRapport = (rapport) => {
+    let headers = getHeaders();
+    headers['Content-Type'] =  'application/json';
+    fetch(`${URL}/rapports`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(rapport)
+    }).then((result) => {
+        result.json().then((rapport) => {
+            rapports.push(rapport);
+            renderRapports();
         });
     });
 };
@@ -69,6 +105,25 @@ const deleteEntry = (id) => {
     });
 };
 
+const deleteProject = (id) => {
+    let headers = getHeaders();
+    fetch(`${URL}/projects/${id}`, {
+        method: 'DELETE',
+        headers: headers
+    }).then((result) => {
+        indexProjects();
+    });
+};
+
+const deleteRapport = (id) => {
+    let headers = getHeaders();
+    fetch(`${URL}/rapports/${id}`, {
+        method: 'DELETE',
+        headers: headers
+    }).then((result) => {
+        indexRapports();
+    });
+};
 
 const updateEntry = (entry) => {
     let headers = getHeaders();
@@ -85,21 +140,75 @@ const updateEntry = (entry) => {
     });
 }
 
+const updateProject = (project) => {
+    let headers = getHeaders();
+    headers['Content-Type'] = 'application/json';
+    fetch(`${URL}/projects/${project.id}`, {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(project)
+    }).then((result) => {
+        result.json().then((project) => {
+            projects = projects.map((e) => e.id === project.id ? project : e);
+            renderProjects();
+        });
+    });
+}
 
-// Rendering
-const resetForm = () => {
+const updateRapport = (rapport) => {
+    let headers = getHeaders();
+    headers['Content-Type'] = 'application/json';
+    fetch(`${URL}/rapports/${rapport.id}`, {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(rapport)
+    }).then((result) => {
+        result.json().then((rapport) => {
+            rapports = rapports.map((e) => e.id === rapport.id ? rapport : e);
+            renderRapports();
+        });
+    });
+}
+
+
+const resetEntryForm = () => {
     const entryForm = document.querySelector('#createEntryForm');
     entryForm.reset();
-    mode = 'create';
+    entryMode = 'create';
     currentEntry = null;
 }
 
-//"3" = 3 --> typen können abweichen bei ==
+const resetProjectForm = () => {
+    const projectForm = document.querySelector('#createProjectForm');
+    projectForm.reset();
+    projectMode = 'create';
+    currentProject = null;
+}
+
+const resetRapportForm = () => {
+    const rapportForm = document.querySelector('#createRapportForm');
+    rapportForm.reset();
+    rapportMode = 'create';
+    currentRapport = null;
+}
+
+
 const getUserById = (id) => {
     return users.find((user) => {return user.id == id});
 }
 
-const saveForm = (e) => {
+const getProjectById = (id) => {
+    return projects.find((project) => {return project.id == id});
+}
+
+const getRapportById = (id) => {
+    return rapports.find((rapport) => {return rapport.id == id});
+}
+
+
+
+
+const saveEntryForm = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const entry = {};
@@ -107,17 +216,50 @@ const saveForm = (e) => {
     entry['checkOut'] = dateAndTimeToDate(formData.get('checkOutDate'), formData.get('checkOutTime'));
     entry['user'] = getUserById(formData.get('user'));
 
-    if (mode === 'create') {
+    if (entryMode === 'create') {
         createEntry(entry);
     } else {
         entry.id = currentEntry.id;
         updateEntry(entry);
     }
-    resetForm();
+    resetEntryForm();
 }
 
+const saveRapportForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const rapport = {};
+    rapport['user'] = getUserById(formData.get('user'));
+    rapport['project'] = getProjectById(formData.get('project'));
+    rapport['workload'] = formData.get('workload');
+
+    if (rapportMode === 'create') {
+        createRapport(rapport);
+    } else {
+        rapport.id = currentEntry.id;
+        updateRapport(rapport);
+    }
+    resetRapportForm();
+}
+
+const saveProjectForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const project = {};
+    project['identifier'] = formData.get('identifier');
+
+    if (projectMode === 'create') {
+        createProject(project);
+    } else {
+        project.id = currentProject.id;
+        updateProject(project);
+    }
+    resetProjectForm();
+}
+
+
 const editEntry = (entry) => {
-    mode = 'edit';
+    entryMode = 'edit';
     currentEntry = entry;
     const entryForm = document.querySelector('#createEntryForm');
     const checkInDateField = entryForm.querySelector('[name="checkInDate"]');
@@ -132,7 +274,28 @@ const editEntry = (entry) => {
     userSelectField.value = entry.user.id;
 }
 
-const createActions = (entry) => {
+const editRapport = (rapport) => {
+    rapportMode = 'edit';
+    currentRapport = rapport;
+    const rapportForm = document.querySelector('#createRapportForm');
+    const userSelectField = rapportForm.querySelector('[name="user"]');
+    userSelectField.value = rapport.user.id;
+    const projectSelectField = rapportForm.querySelector('[name="project"]');
+    projectSelectField.value = rapport.project.id;
+    const workloadField = rapportForm.querySelector('[name="workload"]');
+    workloadField.value = rapport.workload;
+}
+
+const editProject = (project) => {
+    projectMode = 'edit';
+    currentProject = project;
+    const projectForm = document.querySelector('#createProjectForm');
+    const identifierField = projectForm.querySelector('[name="identifier"]');
+    identifierField.value = project.identifier;
+}
+
+
+const createEntryActions = (entry) => {
     const cell = document.createElement('td');
     const deleteButton = document.createElement('button');
     deleteButton.innerText = 'Delete';
@@ -144,6 +307,34 @@ const createActions = (entry) => {
     cell.appendChild(editButton);
     return cell;
 }
+
+
+const createRapportActions = (rapport) => {
+    const cell = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => deleteRapport(rapport.id));
+    cell.appendChild(deleteButton);
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => editRapport(rapport));
+    cell.appendChild(editButton);
+    return cell;
+}
+
+const createProjectActions = (project) => {
+    const cell = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => deleteProject(project.id));
+    cell.appendChild(deleteButton);
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => editProject(project));
+    cell.appendChild(editButton);
+    return cell;
+}
+
 
 const indexEntries = () => {
     let headers = getHeaders();
@@ -159,6 +350,36 @@ const indexEntries = () => {
     renderEntries();
 };
 
+const indexProjects = () => {
+    let headers = getHeaders();
+    fetch(`${URL}/projects`, {
+        method: 'GET',
+        headers: headers
+    }).then((result) => {
+        result.json().then((result) => {
+            projects = result;
+            renderProjects();
+            populateProjectsDropdown();
+        });
+    });
+    renderProjects();
+};
+
+const indexRapports = () => {
+    let headers = getHeaders();
+    fetch(`${URL}/rapports`, {
+        method: 'GET',
+        headers: headers
+    }).then((result) => {
+        result.json().then((result) => {
+            rapports = result;
+            renderRapports();
+        });
+    });
+    renderRapports();
+};
+
+
 const indexUsers = () => {
     let headers = getHeaders();
     fetch(`${URL}/users`, {
@@ -167,27 +388,51 @@ const indexUsers = () => {
     }).then((result) => {
         result.json().then((result) => {
             users = result;
-            populateUserDropdown()
-            console.log('Users: ');
-            console.log(users);
+            populateUserDropdown();
         });
     });
 };
 
-
 const populateUserDropdown = () => {
-    const usersDropdwon = document.querySelector('#userselect');
+    const usersDropdownEntry = document.querySelector('#userSelectEntry');
+    const usersDropdownRapport = document.querySelector('#userSelectRapport');
     //Create and append the options
+    for (var i = usersDropdownEntry.options.length-1; i>=0; i--){
+        usersDropdownEntry.remove(i);
+    }
+
+    for (var i = usersDropdownRapport.options.length-1; i>=0; i--){
+        usersDropdownRapport.remove(i);
+    }
+
     for (var i = 0; i < users.length; i++) {
-        var option = document.createElement("option");
-        option.value = users[i]['id'];
-        option.text = users[i]['username'];
-        usersDropdwon.appendChild(option);
+        var optionEntry = document.createElement("option");
+        optionEntry.value = users[i]['id'];
+        optionEntry.text = users[i]['username'];
+        usersDropdownEntry.appendChild(optionEntry);
+
+        var optionRapport = document.createElement("option");
+        optionRapport.value = users[i]['id'];
+        optionRapport.text = users[i]['username'];
+        usersDropdownRapport.appendChild(optionRapport);
     }
 }
 
+const populateProjectsDropdown = () => {
+    const projectsDropdownRapport = document.querySelector('#projectSelectRapport');
 
+    for (var i = projectsDropdownRapport.options.length-1; i>=0; i--){
+        projectsDropdownRapport.remove(i);
+    }
 
+    //Create and append the options
+    for (var i = 0; i < projects.length; i++) {
+        var optionRapport = document.createElement("option");
+        optionRapport.value = projects[i]['id'];
+        optionRapport.text = projects[i]['username'];
+        projectsDropdownRapport.appendChild(optionRapport);
+    }
+}
 
 const createCell = (text) => {
     const cell = document.createElement('td');
@@ -204,10 +449,39 @@ const renderEntries = () => {
         row.appendChild(createCell(entry.user.username));
         row.appendChild(createCell(new Date(entry.checkIn).toLocaleString()));
         row.appendChild(createCell(new Date(entry.checkOut).toLocaleString()));
-        row.appendChild(createActions(entry));
+        row.appendChild(createEntryActions(entry));
         display.appendChild(row);
     });
 };
+
+
+const renderRapports = () => {
+    const display = document.querySelector('#rapportDisplay');
+    display.innerHTML = '';
+    rapports.forEach((rapport) => {
+        const row = document.createElement('tr');
+        row.appendChild(createCell(rapport.id));
+        row.appendChild(createCell(rapport.user.username));
+        row.appendChild(createCell(rapport.project.identifier));
+        row.appendChild(createCell(rapport.workload));
+        row.appendChild(createRapportActions(rapport));
+        display.appendChild(row);
+    });
+};
+
+const renderProjects = () => {
+    const display = document.querySelector('#projectDisplay');
+    display.innerHTML = '';
+    projects.forEach((project) => {
+        const row = document.createElement('tr');
+        row.appendChild(createCell(project.id));
+        row.appendChild(createCell(project.identifier));
+        row.appendChild(createProjectActions(project));
+        display.appendChild(row);
+    });
+};
+
+
 
 const checkAuthorized = () => {
     if (localStorage.getItem('bearer') === null) {
@@ -227,13 +501,25 @@ document.addEventListener('DOMContentLoaded', function(){
     //createEntryForm.addEventListener('submit', createEntry);
 
     const entryForm = document.querySelector('#createEntryForm');
-    entryForm.addEventListener('submit', saveForm);
-    entryForm.addEventListener('reset', resetForm);
+    entryForm.addEventListener('submit', saveEntryForm);
+    entryForm.addEventListener('reset', resetEntryForm);
     indexEntries();
+
+    const projectForm = document.querySelector('#createProjectForm');
+    projectForm.addEventListener('submit', saveProjectForm);
+    projectForm.addEventListener('reset', resetProjectForm);
+    indexProjects();
+
+    const rapportForm = document.querySelector('#createRapportForm');
+    rapportForm.addEventListener('submit', saveRapportForm);
+    rapportForm.addEventListener('reset', saveRapportForm);
+    indexRapports();
+
 
     const logoutButton = document.querySelector('#logout-button');
     logoutButton.addEventListener('click', logout);
 
     indexUsers();
     populateUserDropdown();
+    populateProjectsDropdown();
 });
